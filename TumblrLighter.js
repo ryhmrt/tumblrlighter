ff7d45fb601d0c6f67dfa99b4b9b0a7256b9909c
Ryohei Morita <ryhmrt@gmail.com>
2011-06-12 20:23:09 +0900

    fixed scroll position.

--- TumblrLighter.js
+++ TumblrLighter.js
@@ -1,21 +1,33 @@
 (function(){
   var posts = document.getElementById('posts');
   window.addEventListener('AutoPatchWork.append', function(){
-    var y = document.documentElement.scrollTop || document.body.scrollTop;
+    var y = document.body.scrollTop;
     var children = posts.childNodes;
     console.log('posts: ' + children.length);
     var removing = new Array();
+    var current;
     for (var i = 0; i < children.length; i++) {
       var post = children[i];
       if (post.id === 'new_post') continue;
       if (children.length - i > 100) {
-        removing.push(children[i]);
+        removing.push(post);
+        console.log('tagName: ' + post.tagName);
+        console.log('id: ' + post.id);
+        console.log('offsetTop: ' + post.offsetTop);
+        console.log('offsetHeight: ' + post.offsetHeight);
       } else {
-        break;
+        if (!current && post.offsetTop > y) {
+          current = post;
+        }
       }
     }
     for (var i = 0; i < removing.length; i++) {
       posts.removeChild(removing[i]);
+    }
+    if (current) {
+      var newY = current.offsetTop - 10;
+      console.log('scrollTop: ' + newY);
+      document.body.scrollTop = newY;
     }
   }, false);
 })();

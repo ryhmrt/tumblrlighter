88de7a949abf287c47d89263f35ac0484225fc2a
Ryohei Morita <ryhmrt@gmail.com>
2011-07-03 22:04:23 +0900

    improved stability. no longer uses AutoPatchWork.DOMNodeInserted event.

--- TumblrLighter.js
+++ TumblrLighter.js
@@ -1,14 +1,18 @@
 (function(){
+
   var posts = document.getElementById('posts');
-  var loading;
-  window.addEventListener('AutoPatchWork.DOMNodeInserted', function(){
-    if (loading) return;
-    loading = true;
+  var step1, step2;
+  var removing;
+  var current;
+
+  window.addEventListener('AutoPatchWork.append', function(){
+    if (step1) return;
+    step1 = true;
     var y = document.body.scrollTop;
     var children = posts.childNodes;
     console.log('posts: ' + children.length);
-    var removing = new Array();
-    var current;
+    removing = new Array();
+    current = null;
     for (var i = 0; i < children.length; i++) {
       var post = children[i];
       console.log('i: ' + i);
@@ -17,11 +21,11 @@
       console.log('offsetTop: ' + post.offsetTop);
       console.log('offsetHeight: ' + post.offsetHeight);
       if (post.id === 'new_post') continue;
-      if (children.length - i > 100) {
+      if (children.length - i > 50) {
         removing.push(post);
       } else {
         console.log('tagName: ' + post.tagName);
-        if (post.tagName == 'LI') {
+        if (post.tagName == 'LI' && post.id.match('post_.+')) {
           current = post;
         }
         if (post.offsetTop > y) {
@@ -29,18 +33,30 @@
         }
       }
     }
-    for (var i = 0; i < removing.length; i++) {
-      posts.removeChild(removing[i]);
-    }
-    if (current) {
-      window.setTimeout(function(){
+  }, false);
+
+  window.addEventListener('AutoPatchWork.pageloaded', function(){
+    if (step2) return;
+    step2 = true;
+    window.setTimeout(function(){
+      for (var i = 0; i < removing.length; i++) {
+        if (current) {
+          document.body.scrollTop = current.offsetTop - 10;
+        }
+        posts.removeChild(removing[i]);
+      }
+      if (current) {
+        console.log('id: ' + current.id);
+        console.log('offsetTop: ' + current.offsetTop);
         var newY = current.offsetTop - 10;
         console.log('scrollTop: ' + newY);
         document.body.scrollTop = newY;
-        loading = false;
-      }, 50);
-    } else {
-      console.log('no scroll');
-    }
+      } else {
+        console.log('no scroll');
+      }
+      step1 = false;
+      step2 = false;
+    }, 50);
   }, false);
+
 })();
